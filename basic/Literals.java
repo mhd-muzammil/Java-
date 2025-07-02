7526fb0cf4ab7b47fccf429f2403f0b3c39857ec
mhd-muzammil <zamil627810@gmail.com>
2025-07-02 13:55:58 +0530

    Literals

---
+++ basic/Literals.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Literals {
+    public static void main(String[] args) {
+        int a = 7; //7 is a int literal
+        char c = 'd'; //d is a char literals
+        double b = 10; //10 is a double literals
+        boolean f = false; // false is a boolean literals
+
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(f);
+
+    }
+}
